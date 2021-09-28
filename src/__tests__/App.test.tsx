bdbceebb2efb545c7732abc503d653227b3368c0
Per Holte <phholte@stud.ntnu.no>
2021-09-28 16:44:08 +0200

    the start of some tests

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,9 +1,39 @@
-import React from "react"
-import { render, screen } from "@testing-library/react"
-import App from "../App"
+import { cleanup, render, screen } from "@testing-library/react"
+import App from '../App'
+import { BrowserRouter } from "react-router-dom"
+import { act } from "react-dom/test-utils"
 
-test("renders learn react link", () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+beforeEach(() => {
+  return render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>)
+  }
+)
+
+afterEach(cleanup)
+
+
+it("renders the header correctly", () => {
+  
+  const button = screen.getByText("Issues")
+  expect(button).toBeInTheDocument()
+})
+
+
+describe("Checking routing logic", () => {
+
+  
+  it("Should not containt an SVG-element yet", () => {
+    expect(document.body).not.toContain("svg")
+  })
+
+  it("Re-routes to branches when branches-link is clicked", async () => {
+    act(() => {
+      const commitLink = screen.getByText("Branches") 
+      commitLink.dispatchEvent(new MouseEvent("click", {bubbles: true}))
+    })
+    const svg = screen.findByRole("svg")
+    await expect(svg).toBeDefined()
+  })
 })

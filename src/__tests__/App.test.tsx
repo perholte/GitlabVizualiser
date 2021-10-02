da2b3b9b19d211b11a3a96547a51477cf1735975
Per Holte <phholte@stud.ntnu.no>
2021-10-02 14:41:29 +0200

    #26 - almost done

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,10 +1,10 @@
 import { cleanup, render, screen } from '@testing-library/react';
-import { jest } from '@jest/globals';
 import App from '../App';
 import { BrowserRouter } from 'react-router-dom';
 import { act } from 'react-dom/test-utils';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import Header from '../components/header/Header';
+import userEvent from '@testing-library/user-event';
 
 // Prosjektet skal vise oppsett av og eksempel på testing med Jest - minimum er å ha en snapshottest og noen enkle tester på komponentenes oppførsel.
 
@@ -18,7 +18,7 @@ beforeEach(() => {
 	);
 });
 afterEach(cleanup);
-
+/*
 it('renders the App correctly', () => {
 	expect(<App></App>).toMatchSnapshot();
 });
@@ -65,4 +65,46 @@ describe('Checking the routing of the header', () => {
 		expect(window.location.pathname).toEqual('/contributors');
 	});
 	//Check that the correct component is showed.
+});
+*/
+describe('testing implementation and representation of sessionstorage', () => {
+	it('Writes a string in the input field and submits it', () => {
+		const inputLabel = screen.getByRole('textbox');
+		const button = screen.getByText('Submit name');
+		userEvent.type(inputLabel, 'test');
+		button.click();
+		expect(sessionStorage.getItem('name')).toEqual('test');
+	});
+
+	it('shows the string on the commit-page', () => {
+		const commitLink = screen.getByText('Commits');
+		commitLink.click();
+
+		const input = screen.getByText('Hello test');
+		expect(input).toBeInTheDocument();
+	});
+
+	it('shows the string on the issue-page', () => {
+		const issueLink = screen.getByRole('button', {name: 'Issues'});
+		issueLink.click();
+
+		const input = screen.getByText('Hello test');
+		expect(input).toBeInTheDocument();
+	});
+
+	it('shows the string on the contributor-page', () => {
+		const contributorLink = screen.getByText('Contributors');
+		contributorLink.click();
+
+		const input = screen.getByText('Hello test');
+		expect(input).toBeInTheDocument();
+	});
+
+	it('shows the string on the branch-page', () => {
+		const branchLink = screen.getByText('Branches');
+		branchLink.click();
+
+		const input = screen.getByText('Hello test');
+		expect(input).toBeInTheDocument();
+	});
 });

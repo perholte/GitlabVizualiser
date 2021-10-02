bd4b9cb19575fbaa9d7dacd2cc478e3930dbaf6c
André Schjøth <aschjoth@gmail.com>
2021-10-02 17:54:15 +0200

    use shared component to reduse amount of redundant code

--- src/components/issues/Issue.tsx
+++ src/components/issues/Issue.tsx
@@ -1,17 +1,8 @@
-import {
-	AccordionButton,
-	AccordionIcon,
-	AccordionItem,
-	AccordionPanel,
-} from '@chakra-ui/accordion';
-import { HStack } from '@chakra-ui/layout';
-import { useStyleConfig } from '@chakra-ui/react';
-import { ComponentStyleConfig } from '@chakra-ui/theme';
-import marked from 'marked';
 import React from 'react';
-import { containerStyles } from '../style/styles';
+import marked from 'marked';
 import './issues.css';
 import parse from 'html-react-parser';
+import AccordionListItem from '../common/AccordionListItem';
 
 //TODO: update this type and maybe move it to a 'type' folder
 export interface IssueType {
@@ -26,48 +17,22 @@ interface IssueProps {
 }
 
 const Issue: React.FC<IssueProps> = ({ data }) => {
-	const styles = useStyleConfig('Issue', {
-		variant: data.closed ? 'closed' : 'open',
-	});
-
 	const descriptionContent = marked(data.description);
 	const descriptionTitle = marked(data.title);
 
 	return (
-		<AccordionItem sx={styles} className='issue_element'>
-			<AccordionButton>
-				<HStack w='100%' justifyContent='space-between'>
-					<HStack fontWeight='bold'>
-						<span style={{ width: '4ch', textAlign: 'left' }}>
-							{'#' + data.id}
-						</span>
-						{parse(descriptionTitle)}
-					</HStack>
-					{data.description && <AccordionIcon />}
-				</HStack>
-			</AccordionButton>
-			{data.description && (
-				<AccordionPanel pb={4}>
-					<div>{parse(descriptionContent)}</div>
-				</AccordionPanel>
-			)}
-		</AccordionItem>
+		<AccordionListItem
+			buttonChildren={
+				<>
+					<span style={{ width: '4ch', textAlign: 'left' }}>
+						{'#' + data.id}
+					</span>
+					{parse(descriptionTitle)}
+				</>
+			}
+			panelChildren={<div>{parse(descriptionContent)}</div>}
+			disablePanel={!!data.description}
+		></AccordionListItem>
 	);
 };
 export default Issue;
-
-export const IssueConfig: ComponentStyleConfig = {
-	baseStyle: {
-		width: '100%',
-		...containerStyles,
-		margin: 'auto',
-	},
-	variants: {
-		open: {
-			background: '#f9f9f9',
-		},
-		closed: {
-			background: '#dedede', //TODO: chance color
-		},
-	},
-};

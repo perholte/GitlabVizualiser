62fbea647a1fed8b8cea7ad02794d7ed739cdc56
André Schjøth <aschjoth@gmail.com>
2021-10-02 22:02:33 +0200

    added closed style

--- src/components/issues/Issue.tsx
+++ src/components/issues/Issue.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import marked from 'marked';
-import './issues.css';
 import parse from 'html-react-parser';
 import AccordionListItem from '../common/AccordionListItem';
+import { Text } from '@chakra-ui/layout';
 
 //TODO: update this type and maybe move it to a 'type' folder
 export interface IssueType {
@@ -16,6 +16,8 @@ interface IssueProps {
 	data: IssueType;
 }
 
+const closedColor = 'rgba(161,40,48,0.7)';
+
 const Issue: React.FC<IssueProps> = ({ data }) => {
 	const descriptionContent = marked(data.description);
 	const descriptionTitle = marked(data.title);
@@ -24,10 +26,21 @@ const Issue: React.FC<IssueProps> = ({ data }) => {
 		<AccordionListItem
 			buttonChildren={
 				<>
-					<span style={{ width: '4ch', textAlign: 'left' }}>
+					<span
+						style={{
+							width: '4ch',
+							textAlign: 'left',
+							color: data.closed ? closedColor : undefined,
+						}}
+					>
 						{'#' + data.id}
 					</span>
-					{parse(descriptionTitle)}
+					<Text
+						textAlign='left'
+						sx={data.closed ? { color: closedColor } : {}}
+					>
+						{parse(descriptionTitle)}
+					</Text>
 				</>
 			}
 			panelChildren={<div>{parse(descriptionContent)}</div>}
